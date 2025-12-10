e3f4fdd98a8c34a702fcd0de2a354e89feef22c9
Ivan Gudelj <ivan.gudelj@protonmail.com>
2025-12-10 22:30:44 +0100

    Initial backend setup

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "running"})
+	})
+
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
